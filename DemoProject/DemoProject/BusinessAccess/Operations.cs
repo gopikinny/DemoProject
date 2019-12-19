1f7899ccfd7965ecb425c1932928ac1067b7ea46
gopikinny <gopikrishna.vtalent@gmail.com>
2019-12-19 17:42:45 +0530

    Addition Method Operation Class Added to the Bal Folder

---
+++ DemoProject/DemoProject/BusinessAccess/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoProject.BusinessAccess
+{
+    public class Operations
+    {
+        public int Add()
+        {
+            int a = 10;
+            int b = 20;
+            return (a + b);
+        }
+    }
+}
